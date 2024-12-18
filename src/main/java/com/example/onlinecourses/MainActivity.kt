075ce1e243cba76ffe0808b1cf470ad1c3493573
kamend119 <a.mikryukova119@mail.ru>
2024-12-18 22:30:58 +0500

    авторизация регистрация и главная входа

--- src/main/java/com/example/onlinecourses/MainActivity.kt
+++ src/main/java/com/example/onlinecourses/MainActivity.kt
@@ -1,104 +1,159 @@
 package com.example.onlinecourses
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.onlinecourses.account.Account
+import com.example.onlinecourses.account.EditAccount
+import com.example.onlinecourses.administrator.MainAdministrator
+import com.example.onlinecourses.administrator.coursesManagement.CoursesManagement
+import com.example.onlinecourses.administrator.coursesManagement.LectureManagement
+import com.example.onlinecourses.administrator.coursesManagement.MainCoursesManagement
+import com.example.onlinecourses.administrator.support.AppealsAdministrator
+import com.example.onlinecourses.administrator.support.SupportsAdministrator
+import com.example.onlinecourses.administrator.userManagement.MainUserManagement
+import com.example.onlinecourses.administrator.userManagement.UserManagement
+import com.example.onlinecourses.courseOwner.MainOwner
+import com.example.onlinecourses.courseOwner.MyCourses
+import com.example.onlinecourses.courseOwner.Statistics
+import com.example.onlinecourses.courseOwner.checkingProgress.AnswerUser
+import com.example.onlinecourses.courseOwner.checkingProgress.MainChecking
+import com.example.onlinecourses.courseOwner.checkingProgress.UserResponses
+import com.example.onlinecourses.courseOwner.createCourse.CreateAnswer
+import com.example.onlinecourses.courseOwner.createCourse.CreateLecture
+import com.example.onlinecourses.courseOwner.createCourse.CreateStep
+import com.example.onlinecourses.courseOwner.createCourse.MainCreate
+import com.example.onlinecourses.courseOwner.editCourse.AnswerManagement
+import com.example.onlinecourses.courseOwner.editCourse.EditAnswer
+import com.example.onlinecourses.courseOwner.editCourse.EditLecture
+import com.example.onlinecourses.courseOwner.editCourse.MainEdit
 import com.example.onlinecourses.entrance.Authorization
-import com.example.onlinecourses.ui.theme.OnlineCursesTheme
+import com.example.onlinecourses.entrance.Entrance
+import com.example.onlinecourses.entrance.Registration
+import com.example.onlinecourses.settings.ChangingThePassword
+import com.example.onlinecourses.settings.Settings
+import com.example.onlinecourses.student.CompletedCourses
+import com.example.onlinecourses.student.DeferredCourses
+import com.example.onlinecourses.student.MainStudent
+import com.example.onlinecourses.student.certificates.Certificate
+import com.example.onlinecourses.student.certificates.MainCertificate
+import com.example.onlinecourses.student.myCourses.MainMyCourses
+import com.example.onlinecourses.student.searchCourses.CoursesPreview
+import com.example.onlinecourses.student.searchCourses.MainSearchCourses
+import com.example.onlinecourses.student.takingCourses.Lecture
+import com.example.onlinecourses.student.takingCourses.MainCourses
+import com.example.onlinecourses.student.takingCourses.Question
+import com.example.onlinecourses.support.AddAppeal
+import com.example.onlinecourses.support.Appeal
+import com.example.onlinecourses.support.Support
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Authorization()
+            MyApp()
         }
     }
 }
 
+//  navController.navigate("authorization")
 
 @Composable
-@Preview(showBackground = true, showSystemUi = true)
-fun testsus(){
-    OnlineCursesTheme {
-        AppBarStudent("student") {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(30.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Top
-            ) {
-                Text("Поддержка", style = MaterialTheme.typography.titleMedium)
+fun MyApp() {
+    val navController = rememberNavController()
+    var userId by remember { mutableStateOf("1") }
+    var role by remember { mutableStateOf("Студент") }
 
-                Column(
-                    modifier = Modifier.weight(1f),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                }
-            }
-        }
-    }
-}
-@Composable
-@Preview(showBackground = true, showSystemUi = true)
-fun testow(){
-    OnlineCursesTheme {
-        AppBarCourseOwner("owner") {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(30.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Top
-            ) {
-                Text("Поддержка", style = MaterialTheme.typography.titleMedium)
+    NavHost(navController = navController, startDestination = "entrance") {
+        //entrance
+        composable("entrance") { Entrance(navController) } // готово
+        composable("authorization") { Authorization(navController) } // готово
+        composable("registration") { Registration(navController) } // готово
 
-                Column(
-                    modifier = Modifier.weight(1f),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                }
-            }
-        }
-    }
-}
-@Composable
-@Preview(showBackground = true, showSystemUi = true)
-fun testad(){
-    OnlineCursesTheme {
-        AppBarAdministrator("adnim"){
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(30.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Top
-            ) {
-                Text("admin", style = MaterialTheme.typography.titleMedium)
+        //account
+        composable("account") { Account(navController, userId, role) }
+        composable("editAccount") { EditAccount(navController, userId, role) }
 
-                Column(
-                    modifier = Modifier.weight(1f),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                }
+        //administrator
+        composable("coursesManagement") { CoursesManagement(navController, userId) }
+        composable("answerManagement") { AnswerManagement(navController, userId) }
+        composable("lectureManagement") { LectureManagement(navController, userId) }
+        composable("mainCoursesManagement") { MainCoursesManagement(navController, userId) }
+        composable("appealsAdministrator") { AppealsAdministrator(navController, userId) }
+        composable("supportsAdministrator") { SupportsAdministrator(navController, userId) }
+        composable("mainUserManagement") { MainUserManagement(navController, userId) }
+        composable("userManagement") { UserManagement(navController, userId) }
+        composable("mainAdministrator/{userId}/{role}") { backStackEntry ->
+            userId = backStackEntry.arguments?.getString("userId")?: "1"
+            role = backStackEntry.arguments?.getString("role")?: "Студент"
+            if (userId == "1" && role == "Студент") {
+                Log.e("Navigation", "Аргументы переданы некорректно: userId=$userId, role=$role")
             }
+            MainAdministrator(navController, userId, role)
         }
+
+        //courseOwner
+        composable("answerUser") { AnswerUser(navController, userId) }
+        composable("mainChecking") { MainChecking(navController, userId) }
+        composable("userResponses") { UserResponses(navController, userId) }
+        composable("createAnswer") { CreateAnswer(navController, userId) }
+        composable("createLecture") { CreateLecture(navController, userId) }
+        composable("createStep") { CreateStep(navController, userId) }
+        composable("mainCreate") { MainCreate(navController, userId) }
+        composable("editAnswer") { EditAnswer(navController, userId) }
+        composable("editLecture") { EditLecture(navController, userId) }
+        composable("mainEdit") { MainEdit(navController, userId) }
+        composable("mainOwner/{userId}/{role}") { backStackEntry ->
+            userId = backStackEntry.arguments?.getString("userId")?: "1"
+            role = backStackEntry.arguments?.getString("role")?: "Студент"
+            if (userId == "1" && role == "Студент") {
+                Log.e("Navigation", "Аргументы переданы некорректно: userId=$userId, role=$role")
+            }
+            MainOwner(navController, userId, role)
+        }
+        composable("myCourses") { MyCourses(navController, userId) }
+        composable("statistics") { Statistics(navController, userId) }
+
+        //settings
+        composable("changingThePassword") { ChangingThePassword(navController, userId, role) }
+        composable("settings") { Settings(navController, userId, role) }
+
+        //student
+        composable("certificate") { Certificate(navController, userId) }
+        composable("mainCertificate") { MainCertificate(navController, userId) }
+        composable("mainMyCourses") { MainMyCourses(navController, userId) }
+        composable("coursesPreview") { CoursesPreview(navController, userId) }
+        composable("mainSearchCourses") { MainSearchCourses(navController, userId) }
+        composable("lecture") { Lecture(navController, userId) }
+        composable("mainCourses") { MainCourses(navController, userId) }
+        composable("question") { Question(navController, userId) }
+        composable("completedCourses") { CompletedCourses(navController, userId) }
+        composable("deferredCourses") { DeferredCourses(navController, userId) }
+        composable("mainStudent/{userId}/{role}") { backStackEntry ->
+            userId = backStackEntry.arguments?.getString("userId")?: "1"
+            role = backStackEntry.arguments?.getString("role")?: "Студент"
+            if (userId == "1" && role == "Студент") {
+                Log.e("Navigation", "Аргументы переданы некорректно: userId=$userId, role=$role")
+            }
+            MainStudent(navController, userId, role)
+        }
+
+        //support
+        composable("support") { Support(navController, userId) }
+        composable("appeal") { Appeal(navController, userId) }
+        composable("addAppeal") { AddAppeal(navController, userId) }
 
     }
 }
